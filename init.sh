587eab799f02a22d7e280b13ad391b7153b507bd
Diego Drese <diego.neumann.drese@gmail.com>
2021-10-07 11:10:12 -0300

    Ajustando para projeto hermes

--- init.sh
+++ init.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+PROJECT=""
+PACKAGE=""
+INSTALL_ALL=""
+
+function usage() {
+    echo "Allowed commands"
+    echo ""
+    echo "--help"
+    echo "--install-all=true "
+    echo "--project=hermes | bisc8 "
+    echo "--package=ironforge | hermes | bisc8 | belit | beli2  "
+    echo ""
+}
+
 ID_RSA=".ssh/id_rsa.pub";
 DIVIDER="###################################"
 if [ ! -f "$ID_RSA" ]; then
@@ -11,76 +25,105 @@ if [ ! -f "$ID_RSA" ]; then
   echo "Add git as trusted";
   ssh-keyscan -H github.com >> ~/.ssh/known_hosts
   echo $DIVIDER;
-  exit;
 fi
-INSTALL=$1
-if [ "$INSTALL" == 'install' ]; then
+
+while [ ! "$1" == "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        --project)
+            PROJECT=$VALUE
+            ;;
+          --install-all)
+            INSTALL_ALL=$VALUE
+            ;;
+        --package)
+            PACKAGE=$VALUE
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+###PACKAGES
+if [ "$PACKAGE" == "ironforge" ] ||  [ "$INSTALL_ALL" == "true" ]; then
   IRONFORGE='packages/aggrega/ironforge'
   if [ ! -d "$IRONFORGE" ]; then
     git clone git@github.com:AGGREGA/IronforgePkg.git ./packages/aggrega/ironforge
   fi
+fi
+
+if [ "$PACKAGE" == "belit" ] ||  [ "$INSTALL_ALL" == "true" ]; then
   BELIT='packages/aggrega/belit'
   if [ ! -d "$BELIT" ]; then
     git clone git@github.com:AGGREGA/belit.git ./packages/aggrega/belit
   fi
+fi
+
+if [ "$PACKAGE" == "belit2" ] ||  [ "$INSTALL_ALL" == "true" ]; then
   BELIT2='packages/aggrega/belit2'
   if [ ! -d "$BELIT2" ]; then
-    git clone git@github.com:AGGREGA/belit2.git ./packages/aggrega/belit2
+    git clone git@github.com:AGGREGA/IronforgePkg.git ./packages/aggrega/belit2
   fi
+fi
+
+if [ "$PACKAGE" == "hermes" ] ||  [ "$INSTALL_ALL" == "true" ]; then
   HERMES='packages/aggrega/hermes'
   if [ ! -d "$HERMES" ]; then
     git clone git@github.com:AGGREGA/hermes-pkg.git ./packages/aggrega/hermes
   fi
-  BIFROST='packages/aggrega/bifrost'
-  if [ ! -d "$BIFROST" ]; then
-    git clone git@github.com:AGGREGA/bifrost-pkg.git ./packages/aggrega/bifrost
-  fi
+fi
+
+if [ "$PACKAGE" == "bisc8" ] ||  [ "$INSTALL_ALL" == "true" ]; then
   BISC8='packages/aggrega/bisc8'
   if [ ! -d "$BISC8" ]; then
     git clone git@github.com:AGGREGA/bisc8-pkg.git ./packages/aggrega/bisc8
   fi
-  LOKI='packages/aggrega/loki'
-  if [ ! -d "$LOKI" ]; then
-    git clone git@github.com:AGGREGA/loki-pkg.git ./packages/aggrega/loki
-  fi
-  TOTH='packages/aggrega/toth'
-  if [ ! -d "$TOTH" ]; then
-    git clone git@github.com:AGGREGA/toth.git ./packages/aggrega/toth
-  fi
-  WATCHMEN='packages/aggrega/watchmen'
-  if [ ! -d "$WATCHMEN" ]; then
-    git clone git@github.com:AGGREGA/watchmen.git ./packages/aggrega/watchmen
-  fi
-  SESH='packages/aggrega/sesh'
-  if [ ! -d "$SESH" ]; then
-    git clone git@github.com:AGGREGA/sesh.git ./packages/aggrega/sesh
-  fi
+fi
+
+###PROJECTS
+if [ "$PROJECT" == "hermes" ] ||  [ "$INSTALL_ALL" == "true" ]; then
   HERMES_BACKEND='projects/Hermes-backend'
-  if [ ! -d "$HERMES_BACKEND" ]; then
-    git clone git@github.com:AGGREGA/Hermes-backend.git ./projects/Hermes-backend
-    mkdir -p projects/Hermes-backend/packages
-    ln -s ../../../packages/aggrega projects/Hermes-backend/packages/aggrega
-  fi
-  HERMES_FRONT='projects/Hermes-front'
-  if [ ! -d "$HERMES_BACKEND" ]; then
-    git clone git@github.com:AGGREGA/Hermes-front.git ./projects/Hermes-front
-    mkdir -p projects/Hermes-front/packages
-    ln -s ../../../packages/aggrega projects/Hermes-front/packages/aggrega
-  fi
-  HERMES_PRINT='projects/Hermes-print'
-  if [ ! -d "$HERMES_PRINT" ]; then
-    git clone git@github.com:AGGREGA/node-print-page.git ./projects/Hermes-print
-  fi
-  HERMES_HEATMAP='projects/Hermes-heatmap'
-  if [ ! -d "$HERMES_HEATMAP" ]; then
-    git clone git@github.com:AGGREGA/node-heatmap.git ./projects/Hermes-heatmap
-  fi
-  BISC8='projects/Bisc8'
-  if [ ! -d "$BISC8" ]; then
-    git clone git@github.com:AGGREGA/bisc8-front.git ./projects/Bisc8
-    mkdir -p projects/Bisc8/packages
-    ln -s ../../../packages/aggrega projects/Hermes-front/packages/aggrega
-  fi
+    if [ ! -d "$HERMES_BACKEND" ]; then
+      git clone git@github.com:AGGREGA/Hermes-backend.git ./projects/Hermes-backend
+      mkdir -p projects/Hermes-backend/packages
+      ln -s ../../../packages/aggrega projects/Hermes-backend/packages/aggrega
+      cp projects/Hermes-backend/.env.example projects/Hermes-backend/.env
+    fi
+
+    HERMES_FRONT='projects/Hermes-front'
+      if [ ! -d "$HERMES_FRONT" ]; then
+        git clone git@github.com:AGGREGA/Hermes-front.git ./projects/Hermes-front
+        mkdir -p projects/Hermes-front/packages
+        ln -s ../../../packages/aggrega projects/Hermes-front/packages/aggrega
+        cp  projects/Hermes-front/.env.example  projects/Hermes-front/.env
+      fi
+
+    HERMES_PRINT='projects/Hermes-print'
+      if [ ! -d "$HERMES_PRINT" ]; then
+        git clone git@github.com:AGGREGA/node-print-page.git ./projects/Hermes-print
+      fi
+      HERMES_HEATMAP='projects/Hermes-heatmap'
+      if [ ! -d "$HERMES_HEATMAP" ]; then
+        git clone git@github.com:AGGREGA/node-heatmap.git ./projects/Hermes-heatmap
+      fi
+fi
+
+if [ "$PROJECT" == "hermes" ] ||  [ "$INSTALL_ALL" == "true" ]; then
+    BISC8='projects/Bisc8'
+      if [ ! -d "$BISC8" ]; then
+        git clone git@github.com:AGGREGA/bisc8-front.git ./projects/Bisc8
+        mkdir -p projects/Bisc8/packages
+        ln -s ../../../packages/aggrega projects/Hermes-front/packages/aggrega
+      fi
 fi
 
 
